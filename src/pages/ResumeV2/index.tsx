d1b324aed0dfce2ff0b891a5076600415a7af7a2
giselledunine <sophia.hmamouche@outlook.com>
2024-03-08 00:49:59 +0100

    fix typescript errors

--- src/pages/ResumeV2/index.tsx
+++ src/pages/ResumeV2/index.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { Waypoint } from "react-waypoint";
 import moment from "moment";
 import "moment/dist/locale/fr";
 moment.locale("fr");
@@ -48,41 +46,9 @@ export default function Resume() {
 
     const years = [2023, 2022, 2021, 2020];
 
-    const ListTitle = () => {
-        const [listVisible, setListVisible] = useState("visible");
-
-        const handleListDisapear = (e: Waypoint.CallbackArgs) => {
-            const { currentPosition } = e;
-            console.log("currentPostion", currentPosition);
-            if (currentPosition === "above") {
-                setListVisible("hidden");
-            } else if (currentPosition === "inside") {
-                setListVisible("visible");
-            }
-        };
-        return (
-            <Waypoint
-                topOffset={223.5}
-                onPositionChange={(e) => handleListDisapear(e)}>
-                <div className={`list`}></div>
-            </Waypoint>
-        );
-    };
-
     const YearTimeline = () => {
         const yearHeight: { [currentYear: number]: number } = {};
         const experienceHeight: { [id: string]: number } = {};
-        const [heightTable, setHeightTable] = useState([]);
-
-        // useEffect(() => {
-        //     const test = experiences.map((exp) => {
-        //         const divHeight = document.getElementById(exp.id)?.offsetHeight;
-        //         console.log("divHeight", divHeight);
-        //         return { id: exp.id, height: divHeight };
-        //     });
-        //     console.log("test", test);
-        //     setHeightTable(test);
-        // }, []);
 
         experiences.forEach((experience) => {
             const duration =
@@ -103,68 +69,6 @@ export default function Resume() {
             ).fill(0);
 
             console.log("yearLap", yearLap);
-
-            yearLap.map((lap, idx) => {
-                let diff = 0;
-                const currentYear =
-                    Number(moment(experience.date.start).format("YYYY")) + idx;
-                if (yearLap.length > 1) {
-                    if (idx === 0) {
-                        diff = Math.round(
-                            moment(
-                                `${moment(experience.date.start).format(
-                                    "YYYY"
-                                )}-12`
-                            ).diff(
-                                moment(experience.date.start).add(1),
-                                "months",
-                                true
-                            )
-                        );
-                    } else if (idx === yearLap.length - 1) {
-                        diff = Math.round(
-                            moment(experience.date.end).diff(
-                                moment(
-                                    `${moment(experience.date.end)
-                                        .subtract(1, "years")
-                                        .format("YYYY")}-12`
-                                ).add(1),
-                                "months",
-                                true
-                            )
-                        );
-                    } else {
-                        diff = 12;
-                    }
-                }
-
-                // const current1MonthHeight =
-                //     heightTable.find((el) => (el.id = experience.id))?.height /
-                //     duration;
-
-                // console.log("current1MonthHeight", current1MonthHeight);
-                // console.log("currentYear", currentYear);
-
-                // yearHeight[currentYear] = yearHeight[currentYear]
-                //     ? yearHeight[currentYear]
-                //     : 0;
-
-                // console.log(
-                //     "height",
-                //     yearLap.length > 1
-                //         ? current1MonthHeight * diff
-                //         : current1MonthHeight * duration
-                // );
-
-                // yearHeight[currentYear] =
-                //     yearLap.length > 1
-                //         ? yearHeight[currentYear] + current1MonthHeight
-                //             ? current1MonthHeight * diff
-                //             : 0
-                //         : yearHeight[currentYear] + current1MonthHeight
-                //         ? current1MonthHeight * duration
-                //         : 0;
-            });
         });
 
         console.log("yearHeaight", yearHeight);
@@ -193,7 +97,7 @@ export default function Resume() {
                             <div id={exp.id} className="experiences">
                                 <div key={idx}>
                                     <div className="title-timestamp">
-                                        <ListTitle />
+                                        <div className={`list`}></div>
                                         <span className="title">
                                             {exp.title}
                                         </span>
